30a3f8530954a04fd3eb35a20a2f7ff42b4e9cd6
Fajar Ihsana <67202122+Zynfinity@users.noreply.github.com>
2023-02-05 23:34:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
 const server = express()
-var port = 8080;
+var port = 4000;
 server.set("json spaces",2)
 async function y1s(type, url, quality){
     quality = quality ? quality : "360p";
